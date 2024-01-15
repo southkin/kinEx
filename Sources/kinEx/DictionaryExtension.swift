222691186b8308695e0af61b9a49afbe340d5659
kin <south.kin@gmail.com>
2024-01-16 02:20:10 +0900

    dict To Markdown

--- Sources/kinEx/DictionaryExtension.swift
+++ Sources/kinEx/DictionaryExtension.swift
@@ -10,4 +10,25 @@ public extension Dictionary where Key == String, Value == Any {
     var data:Data? {
         return try? JSONSerialization.data(withJSONObject: self, options: [])
     }
+    var markdown:String {
+        return convertToMarkdown(from: self)
+    }
+    fileprivate func convertToMarkdown(from dictionary: [String: Any], indentLevel: Int = 0) -> String {
+        var markdownString = ""
+        let indent = String(repeating: "  ", count: indentLevel)
+        for (key, value) in dictionary {
+            if let subDictionary = value as? [String: Any] {
+                markdownString += "\(indent)- *\(key)*:\n" + convertToMarkdown(from: subDictionary, indentLevel: indentLevel + 1)
+            } else if let array = value as? [Any] {
+                markdownString += "\(indent)- *\(key)*:\n"
+                for item in array {
+                    markdownString += "\(indent)  1. \(item)\n"
+                }
+            } else {
+                markdownString += "\(indent)- **\(key)**: `\(value)`\n"
+            }
+        }
+        return markdownString
+    }
+
 }
